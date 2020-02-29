2d8cafd5fd4cf9729966534544c5f955565a4977
dilaxn128 <dilaxn@outlook.com>
2020-02-29 11:29:55 +0530

    first commit

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ res.sendFile(__dirname+"/fail.html");
 app.post("/fail",function(req,res){
   res.redirect("/");
 })
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("Server is running on port 3000");
 })
 
